7d06aeb2a97515ad1c99890b1c2a2566fa995b54
arenadevel0 <arenadevel0@gmail.com>
2017-03-23 02:47:52 +0530

    Display Message Fixed

--- TestGit.java
+++ TestGit.java
@@ -1,9 +1,5 @@
 public class TestGit{
 	public static void main(String[] args){
-<<<<<<< HEAD
 		System.out.println("Hello GitHub!!");
-=======
-		System.out.println("Hello GitHub.");
->>>>>>> a841ab91f61878cf6188e87d5f8ab1073b64d5d1
 	}
 }
