3dd8953501c7d3a293ee5002cbbccab76cef84b9
arenadevel0 <arenadevel0@gmail.com>
2017-03-23 03:18:06 +0530

    Line 2 added

--- TestGit.java
+++ TestGit.java
@@ -2,5 +2,6 @@ public class TestGit{
 	public static void main(String[] args){
 		System.out.println("Hello GitHub!!");
 		System.out.println("GitHub seems Fascinating!!");
+		System.out.println("GitHub seems Amazing!!");
 	}
 }
